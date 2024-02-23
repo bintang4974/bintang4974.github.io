fcd1e948d5b983848f6bcd026b713a23eb23ad28
bintang4974 <bintangramadhan4974@gmail.com>
2024-02-23 21:39:47 +0700

    Configure React app for deployment to GitHub Pages

---
+++ src/data/constants.js
@@ -0,0 +1,231 @@
+import qazir from '../images/qazir.png';
+import terminal from '../images/terminal.png';
+import gofarm from '../images/gofarm.png';
+
+export const Bio = {
+  name: "Bintang Ramadhan",
+  roles: [
+    "Web Developer",
+    "Programmer",
+  ],
+  description:
+    "As a six-semester Information Systems major, my passion lies in web development and mobile app development. My dedication to staying current with technology, combined with strong work ethics and creativity, makes me an asset to any team or project. I am committed to contributing my skills and passion to innovative software engineering endeavors",
+  github: "https://github.com/bintang4974",
+  resume:
+    "https://drive.google.com/file/d/1hO2FQanZFoxqV8E3fJXORM6jbwc63y2P/view?usp=sharing",
+  // linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+  // twitter: "https://twitter.com/RishavChanda",
+  insta: "https://www.instagram.com/bintangrmd18_/",
+  facebook: "https://www.facebook.com/bintang.r.779205/",
+};
+
+export const skills = [
+  {
+    title: "Web Development",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Codeigniter",
+        image:
+          "https://www.ambientinfotech.com/wp-content/uploads/2020/10/cordinator-logo.png",
+      },
+      {
+        name: "Laravel",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/985px-Laravel.svg.png",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "MySql",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://storage.googleapis.com/neat-element-283712.appspot.com/company/logo/19870/20221123_JC1645524930061_Inosoft-Kotak-1.png",
+    role: "Junior Web Developer Intern",
+    company: "PT. Inosoft Trans Sistem",
+    date: "Aug 2019 - Mar 2020",
+    desc: "Collaborate with the team to create websites.",
+    skills: [
+      "Codeigniter",
+      "MySql",
+      "JavaScript",
+      "Bootstrap",
+      "HTML",
+      "CSS",
+    ],
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZqF3a7Py5E6VuFJZiuSYJhNCFGOL4-hhKht0zvkzjqQ&s",
+    role: "Programming Framework Assistant Lecture",
+    company: "Telkom University Surabaya",
+    date: "Feb 2024 - Jun 2024",
+    desc: "Assist lecturers in teaching lab courses, creating practical questions, and grading assignments",
+    skills: [
+      "Laravel",
+      "MySql",
+      "JavaScript",
+      "Bootstrap",
+      "HTML",
+      "CSS",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZqF3a7Py5E6VuFJZiuSYJhNCFGOL4-hhKht0zvkzjqQ&s",
+    role: "Algorithm & Programming Assistant Lecture",
+    company: "Rudraksha Welffare Foundation",
+    date: "Sep 2022 - Mar 2023",
+    desc: "Assist lecturers in teaching lab courses, creating practical questions, and grading assignments. Conducted a lab course in Algorithms and Programming for the Information System program. Instructed fundamental Python programming with an emphasis on developing problem-solving abilities",
+    skills: [
+      "Python",
+    ],
+  },
+  {
+    id: 3,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQAI8FtGILfYyXBD6COfr4rpiZJK_6Kdebiw7YkiS5LA&s",
+    role: "Teaching Coding Extracurricular",
+    company: "Raudlatul Jannah",
+    date: "Sept 2022",
+    desc: "Develop the talents of students who are interested in the IT field. Students know how a website works and build a simple website",
+    skills: [
+      "HTML",
+      "CSS",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://play-lh.googleusercontent.com/iXGjxp9Xuz6uVabRlfOLn32vwB0hZk2TaLTcL-1CCHfBYFg_suqaauk-mZU7KaqFx5Q",
+    school: "SMKN 1 Surabaya",
+    date: "Oct 2021 - Sep 2025",
+    desc: "Vocational High School (SMK) Graduate in Software Engineering",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZqF3a7Py5E6VuFJZiuSYJhNCFGOL4-hhKht0zvkzjqQ&s",
+    school: "Telkom University Surabaya",
+    date: "Apr 2019 - Apr 2025",
+    desc: "Bachelor Of Information Systems",
+  },
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "Qazir",
+    date: "2023",
+    description:
+      "A Rule Builder application “Decision Hub” that empowers Business Analysts to create, save, and visualize decision strategies. Provide a no-code rule writing experience and visual representation to test these rules in real-time and observe the calculations at each step.",
+    image: qazir,
+    tags: [
+      "Laravel",
+      "MySql",
+      "Javascript",
+      "Bootstrap",
+    ],
+    category: "web app",
+    github: "https://github.com/bintang4974/Qazir",
+  },
+  {
+    id: 9,
+    title: "Terminal Dakwah",
+    date: "2022",
+    description:
+      "Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. \nAdmin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials:	#Email: testemployee@gmail.com	#Password- 123@Testemployee",
+    image: terminal,
+    tags: [
+      "Codeigniter",
+      "MySql",
+      "Javascript",
+      "Bootstrap",
+    ],
+    category: "web app",
+    github: "https://github.com/bintang4974/terminaldakwah",
+    webapp: "https://terminaldakwah.org/",
+  },
+  {
+    id: 1,
+    title: "GoFarm",
+    date: "2023",
+    description:
+      "Designed and developed the Vexa project, a project management app that helps users and teams stay organized and on track. Implemented key features such as task tracking, team collaboration, and time tracking to improve productivity and project outcomes and also invite team/project members. The plan is to add a community of users where they can find developers and other team members and chat system implementation.",
+    image:gofarm,
+    tags: [
+      "React Native",
+      "NativeBase",
+      "Firebase",
+    ],
+    category: "android app",
+    github: "https://github.com/bintang4974/GoFarm",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
